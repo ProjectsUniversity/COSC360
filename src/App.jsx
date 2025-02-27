feeb8829d5ff97d0de57f6dc1fe3c32feb365ca0
Shlok Shah <106793433+shahshlok@users.noreply.github.com>
2025-02-26 19:44:45 -0800

    Remove TailwindCSS from index.css

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
 
   return (
     <div>
-      {!isModalOpen && <button onClick={handleModal}>Open Modal</button>}
+      {!isModalOpen && <button onClick={handleModal}>Open SignUp</button>}
       {isModalOpen && <SignUp onClose={handleModal}/>}
     </div>
   );
