79b92fad1b12bca0c943b52d8a516b140c8f2b9b
Shlok Shah <106793433+shahshlok@users.noreply.github.com>
2025-03-08 21:08:19 -0800

    Refactor App component to remove modal logic and streamline recruiter login flow

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import React, { useState } from 'react'
-import SignUpModal from "./components/SignUp.jsx"
 import SignUp from './components/SignUp.jsx';
 import RecSignUp from './components/recruiters/RecruiterSignUp.jsx';
 import RecruiterLogin from './components/recruiters/RecruiterLogin.jsx';
 
 export default function App() {
-  const[isModalOpen, setIsModalOpen] = useState(false);
   const[showRecruiterLogin, setShowRecruiterLogin] = useState(false);
-
-  function handleModal(){
-    setIsModalOpen(!isModalOpen);
-  }
 
   function toggleRecruiterLogin() {
     setShowRecruiterLogin(!showRecruiterLogin);
@@ -18,13 +12,6 @@ export default function App() {
 
   return (
     <div>
-      {!isModalOpen && !showRecruiterLogin && (
-        <div>
-          <button onClick={handleModal}>Open SignUp</button>
-          <button onClick={toggleRecruiterLogin}>Open Recruiter Login</button>
-        </div>
-      )}
-      {isModalOpen && <SignUp onClose={handleModal}/>}
       {showRecruiterLogin && <RecruiterLogin />}
 
       {!showRecruiterLogin && <RecSignUp />}
